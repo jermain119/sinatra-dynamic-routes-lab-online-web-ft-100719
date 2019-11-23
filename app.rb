82e1f994d346d41399dd214d156e03368d5aa8fb
joshua richards <jermain119@gmail.com>
2019-11-23 18:36:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
 
+  get '/say/:number/:phrase'do
+  (params[:number].to_i ** 2).to_s
+  end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5'do
+  
+  end
+  
+  get '/:operation/:number1/:number2'do
+  
+  end
+  
 end
